d6465b2807ed3b7863f93e20e887704dd19c06e1
lauremegan <lauremegan@hotmail.com>
2012-10-20 12:50:26 +0100

    fixe heroku rejected

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Congolisanga::Application.config.secret_token = 'a014ebcdc9f6eeff5ad026e244b227cac71a417fb77c79ebd7ae9af7077eaf733a06bd863cbc8409b8ef00e99e7cfa7e1cd8f045abefb45105f79533393f5af1'
